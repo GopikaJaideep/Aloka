2651ff901927afeff37857720acda659dce934f0
GopikaJaideep <gopikajaideep201199@gmail.com>
2022-07-16 15:49:29 +0530

    Update

--- app/src/main/java/myapp/t11/aloka/Main.kt
+++ app/src/main/java/myapp/t11/aloka/Main.kt
@@ -3,9 +3,9 @@ package myapp.t11.aloka
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Login : AppCompatActivity() {
+class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main2)
     }
 }
